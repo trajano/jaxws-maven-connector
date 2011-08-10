70bff95ccd6715928855fa8b848427f0f3dfdf2b
bjorn.beskow@callistaenterprise.se <bjorn.beskow@callistaenterprise.se@2471542f-efc0-f505-3bb1-b464c36579b6>
2011-08-10 09:05:10 +0000

    Initial checkin.
    git-svn-id: http://jaxws-maven-connector.googlecode.com/svn/trunk@3 2471542f-efc0-f505-3bb1-b464c36579b6

---
+++ jaxws-connector-plugin/src/com/googlecode/m2e/connectors/jaxws/JaxwsProjectConfigurator.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Callista Enterprise AB - Björn Beskow
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package com.googlecode.m2e.connectors.jaxws;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+
+public class JaxwsProjectConfigurator extends AbstractJavaProjectConfigurator {
+    @Override
+    protected String getOutputFolderParameterName() {
+        return "sourceDestDir";
+    }
+
+    @Override
+    public AbstractBuildParticipant getBuildParticipant(IMavenProjectFacade projectFacade, MojoExecution execution, IPluginExecutionMetadata executionMetadata) {
+        return new JaxwsBuildParticipant(execution);
+    }
+}
