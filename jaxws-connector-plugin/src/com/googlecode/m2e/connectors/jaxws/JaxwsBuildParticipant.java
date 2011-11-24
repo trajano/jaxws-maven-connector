4df55b4b42006c5a84790e2c8abbd8944d2a0670
Archimedes Trajano <arch@trajano.net>
2011-11-23 22:40:40 -0500

    fixed issues found
    added a sample project as well and released the fixes

--- jaxws-connector-plugin/src/com/googlecode/m2e/connectors/jaxws/JaxwsBuildParticipant.java
+++ jaxws-connector-plugin/src/com/googlecode/m2e/connectors/jaxws/JaxwsBuildParticipant.java
@@ -15,6 +15,7 @@ import org.apache.maven.plugin.MojoExecution;
 import org.codehaus.plexus.util.Scanner;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.embedder.IMaven;
 import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
@@ -22,44 +23,55 @@ import org.sonatype.plexus.build.incremental.BuildContext;
 
 public class JaxwsBuildParticipant extends MojoExecutionBuildParticipant {
 
-    public JaxwsBuildParticipant(MojoExecution execution) {
-        super(execution, true);
-    }
+	public JaxwsBuildParticipant(MojoExecution execution) {
+		super(execution, true);
+	}
 
-    @Override
-    public Set<IProject> build(int kind, IProgressMonitor monitor) throws Exception {
-        IMaven maven = MavenPlugin.getMaven();
-        BuildContext buildContext = getBuildContext();
+	@Override
+	public Set<IProject> build(int kind, IProgressMonitor monitor)
+			throws Exception {
+		IMaven maven = MavenPlugin.getMaven();
+		BuildContext buildContext = getBuildContext();
 
-        // check if initial generation has been done
-        File stale = maven.getMojoParameterValue(getSession(), getMojoExecution(), "staleFile", File.class);
-        if (stale != null && stale.exists()) {
-            // check if any of the binding files have changed
-            File source = maven.getMojoParameterValue(getSession(), getMojoExecution(), "bindingDirectory", File.class);
-            Scanner ds = buildContext.newScanner(source); // delta or full scanner
-            ds.scan();
-            String[] includedBindingFiles = ds.getIncludedFiles();
-            // check if any of the wsdl files have changed
-            source = maven.getMojoParameterValue(getSession(), getMojoExecution(), "wsdlDirectory", File.class);
-            ds = buildContext.newScanner(source); // delta or full scanner
-            ds.scan();
-            String[] includedWsdlFiles = ds.getIncludedFiles();
-            // If no changes, simply return
-            if ((includedBindingFiles == null || includedBindingFiles.length <= 0) &&
-                (includedWsdlFiles == null || includedWsdlFiles.length <= 0)) {
-                return null;
-            }
-        }
+		// check if initial generation has been done
+		File stale = maven.getMojoParameterValue(getSession(),
+				getMojoExecution(), "staleFile", File.class);
+		if (stale != null && stale.exists()) {
+			// check if any of the binding files have changed
+			File source = maven.getMojoParameterValue(getSession(),
+					getMojoExecution(), "bindingDirectory", File.class);
+			Scanner ds = buildContext.newScanner(source); // delta or full
+															// scanner
+			try {
+				ds.scan();
+				String[] includedBindingFiles = ds.getIncludedFiles();
+				// check if any of the wsdl files have changed
+				source = maven.getMojoParameterValue(getSession(),
+						getMojoExecution(), "wsdlDirectory", File.class);
+				ds = buildContext.newScanner(source); // delta or full
+														// scanner
+				ds.scan();
+				String[] includedWsdlFiles = ds.getIncludedFiles();
+				// If no changes, simply return
+				if ((includedBindingFiles == null || includedBindingFiles.length <= 0)
+						&& (includedWsdlFiles == null || includedWsdlFiles.length <= 0)) {
+					return null;
+				}
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			}
+		}
 
-        // Otherwise, execute mojo
-        final Set<IProject> result = super.build(kind, monitor);
+		// Otherwise, execute mojo
+		final Set<IProject> result = super.build(kind, monitor);
 
-        // tell m2e builder to refresh generated files
-        File generated = maven.getMojoParameterValue(getSession(), getMojoExecution(), "sourceDestDir", File.class);
-        if (generated != null) {
-            buildContext.refresh(generated);
-        }
+		// tell m2e builder to refresh generated files
+		File generated = maven.getMojoParameterValue(getSession(),
+				getMojoExecution(), "sourceDestDir", File.class);
+		if (generated != null) {
+			buildContext.refresh(generated);
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
---
+++ jaxws-connector-sample-project/target/generated-sources/wsimport/org/example/hello/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org/hello/")
+package org.example.hello;
